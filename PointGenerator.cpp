6ad4dae45f84df252fa8aedfaf1182f4d815ee9b
Daryna Mishyna <dashamishina1509@gmail.com>
2025-10-05 21:44:54 +0300

    Add files via upload

---
+++ PointGenerator.cpp
@@ -0,0 +1,20 @@
+#include "PointGenerator.h"
+
+PointGenerator::PointGenerator(const Point& start, const std::vector<Point>& bases)
+    : current(start), basePoints(bases), gen(std::random_device{}())
+{
+    if (basePoints.empty())
+        throw std::invalid_argument("Список базових точок не може бути порожнім");
+
+    dist = std::uniform_int_distribution<>(0, (int)basePoints.size() - 1);
+}
+
+Point PointGenerator::operator()() {
+    int idx = dist(gen);          
+    const Point& base = basePoints[idx];
+
+    current.x = (current.x + base.x) / 2.0;
+    current.y = (current.y + base.y) / 2.0;
+
+    return current;
+}
